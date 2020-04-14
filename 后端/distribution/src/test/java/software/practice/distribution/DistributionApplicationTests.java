fc086ea905c3569ba3b99bd89c4cea8bffe5f445
Jasonix <ccjjxx99@qq.com>
2020-04-14 17:19:02 +0800

    后端环境搭建，数据库连接

---
+++ 后端/distribution/src/test/java/software/practice/distribution/DistributionApplicationTests.java
@@ -0,0 +1,13 @@
+package software.practice.distribution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
