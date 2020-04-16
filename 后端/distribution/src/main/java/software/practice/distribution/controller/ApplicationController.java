4a28e272fc5c32e10e6f1a168780fff3f7bc79ea
okjer <398384796@qq.com>
2020-04-16 15:26:20 +0800

    zfq完成接口

---
+++ 后端/distribution/src/main/java/software/practice/distribution/controller/ApplicationController.java
@@ -0,0 +1,36 @@
+package software.practice.distribution.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import software.practice.distribution.entity.Application;
+import software.practice.distribution.entity.Community;
+import software.practice.distribution.result.Result;
+import software.practice.distribution.service.ApplicationService;
+import software.practice.distribution.service.CommunityService;
+
+@RestController
+public class ApplicationController {
+    @Autowired
+    ApplicationService applicationService;
+
+    @CrossOrigin
+    @PostMapping(value = "/application/add")
+    public Result createApplication(int application_user,int community_id){
+        Application application = new Application(application_user,community_id);
+        if(applicationService.createApplication(application)){
+            return new Result(200,null);
+        }
+        return new Result(400,"未找到");
+    }
+
+    /*@CrossOrigin
+    @PostMapping(value = "/application/list")
+    public Result getApplicationList(int application_user){
+        if(applicationService.createApplication(application)){
+            return new Result(200,null);
+        }
+        return new Result(400,"未找到");
+    }*/
+}
