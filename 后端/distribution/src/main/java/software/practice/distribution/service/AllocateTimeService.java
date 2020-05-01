9b18aa3e2c8376913b0720fd8bbc64f973d32b9b
okjer <398384796@qq.com>
2020-05-02 00:47:38 +0800

    完成getArrangementsByLocationIdsAndNow函数

--- 后端/distribution/src/main/java/software/practice/distribution/service/AllocateTimeService.java
+++ 后端/distribution/src/main/java/software/practice/distribution/service/AllocateTimeService.java
@@ -7,6 +7,7 @@ import software.practice.distribution.entity.Package;
 
 import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AllocateTimeService {
@@ -24,16 +25,20 @@ public class AllocateTimeService {
         // Arrangement arrangement = new Arrangement();
         //根据package获取user_id
         Integer user_id = p.getPackageUser();
-        //根据userid查询user表，获取community
-        Community community = communityService.getCommunityByUserId(user_id);
-        Integer community_interval = community.getCommunityInterval();
+
         User user = userService.getUserByUserId(user_id);
-        Date favorite_starttime = user.getUserFavoriteStarttime();
+        //根据userId查询user表，获取community
+        Community community = communityService.getDetailById(user.getUserCommunity());
+        Integer community_interval = community.getCommunityInterval();
+
+        Date favorite_startTime = user.getUserFavoriteStarttime();
         //根据community_id查询location表，获取所有location
         List<Location> locations = locationService.getLocationsByCommunity(community.getCommunityId());
-        //根据location_id和当前日期（去掉分秒时）查询arrangement表，
+        //根据location_id和当前日期查询arrangement表，
+        // TODO 去掉分秒时（经过思考，发现不用去掉分秒时，因为不可能安排时间小于now）,去掉分秒时用ArrangementService.roundDownDate()
         // 获取所有arrangement_location in location_id s里,且arrangement_time大于今天日期的arrangement
-        Date now = new Date();
+
+        List<Arrangement> arrangements = arrangementService.getArrangementsByLocationIdsAndNow(locations);
 
 
 
