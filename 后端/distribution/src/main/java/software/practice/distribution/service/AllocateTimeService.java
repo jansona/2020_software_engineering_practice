df79eb089cea6e3313bf80b99a89e700032492a5
pink1999 <969909834@qq.com>
2020-05-01 10:54:49 +0800

    部分，待完成

--- 后端/distribution/src/main/java/software/practice/distribution/service/AllocateTimeService.java
+++ 后端/distribution/src/main/java/software/practice/distribution/service/AllocateTimeService.java
@@ -2,10 +2,11 @@ package software.practice.distribution.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import software.practice.distribution.entity.Arrangement;
+import software.practice.distribution.entity.*;
 import software.practice.distribution.entity.Package;
 
 import java.util.Date;
+import java.util.List;
 
 @Service
 public class AllocateTimeService {
@@ -15,11 +16,26 @@ public class AllocateTimeService {
     LocationService locationService;
     @Autowired
     ArrangementService arrangementService;
+    @Autowired
+    UserService userService;
 
     //可用时间段为早上8点到晚上20点
     public void allocateTime(Package p){
-        //
         // Arrangement arrangement = new Arrangement();
+        //根据package获取user_id
+        Integer user_id = p.getPackageUser();
+        //根据userid查询user表，获取community
+        Community community = communityService.getCommunityByUserId(user_id);
+        Integer community_interval = community.getCommunityInterval();
+        User user = userService.getUserByUserId(user_id);
+        Date favorite_starttime = user.getUserFavoriteStarttime();
+        //根据community_id查询location表，获取所有location
+        List<Location> locations = locationService.getLocationsByCommunity(community.getCommunityId());
+        //根据location_id和当前日期（去掉分秒时）查询arrangement表，
+        // 获取所有arrangement_location in location_id s里,且arrangement_time大于今天日期的arrangement
+        Date now = new Date();
+
+
 
     }
 
--- 后端/distribution/src/main/java/software/practice/distribution/service/LocationService.java
+++ 后端/distribution/src/main/java/software/practice/distribution/service/LocationService.java
@@ -1,7 +1,33 @@
 package software.practice.distribution.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import software.practice.distribution.entity.Location;
+import software.practice.distribution.entity.LocationExample;
+import software.practice.distribution.mapper.LocationMapper;
+import software.practice.distribution.mapper.UserMapper;
+
+import java.util.List;
+
+/**
+ * @author ：Yu Jiayi
+ */
 
 @Service
 public class LocationService {
+
+
+    @Autowired
+    LocationMapper locationMapper;
+
+    //根据community_id查询location表，获取所有location
+    public List<Location> getLocationsByCommunity(int community_id){
+
+        LocationExample locationExample = new LocationExample();
+        LocationExample.Criteria lc = locationExample.createCriteria();
+        lc.andLocationCommunityEqualTo(community_id);
+        return locationMapper.selectByExample(locationExample);
+    }
+
+
 }
