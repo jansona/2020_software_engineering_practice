224ec6c6b700efbfef1025739d9db65156205aa1
Jasonix <ccjjxx99@qq.com>
2020-05-05 20:54:13 +0800

    Update DistributionApplication.java

--- 后端/distribution/src/main/java/software/practice/distribution/DistributionApplication.java
+++ 后端/distribution/src/main/java/software/practice/distribution/DistributionApplication.java
@@ -3,9 +3,11 @@ package software.practice.distribution;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan("software.practice.distribution.mapper")
+@ComponentScan(basePackages = {"software.practice.distribution.controller","software.practice.distribution.service","software.practice.distribution.entity","software.practice.distribution.result","software.practice.distribution.mapper"})
 public class DistributionApplication {
 
     public static void main(String[] args) {
