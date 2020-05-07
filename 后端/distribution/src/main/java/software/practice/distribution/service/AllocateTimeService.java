235ac282a21886c9fb067c39656357dc9b16cad4
pink1999 <969909834@qq.com>
2020-05-07 19:59:32 +0800

    1

--- 后端/distribution/src/main/java/software/practice/distribution/service/AllocateTimeService.java
+++ 后端/distribution/src/main/java/software/practice/distribution/service/AllocateTimeService.java
@@ -81,6 +81,9 @@ public class AllocateTimeService {
 
         Arrangement arrangement;
         Date now = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.set(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH),calendar.get(Calendar.HOUR_OF_DAY)+8,calendar.get(Calendar.MINUTE));
         int index;
         int now_index = transformTimeToIndex(new Date(),community_interval);
         index = Math.max(favorite_index, now_index);
