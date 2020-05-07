45f78841ae5d136329cba65a237fc43f567b64f8
Jasonix <ccjjxx99@qq.com>
2020-05-07 19:21:08 +0800

    时区统一为UTC

--- 后端/distribution/src/main/java/software/practice/distribution/DistributionApplication.java
+++ 后端/distribution/src/main/java/software/practice/distribution/DistributionApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @MapperScan("software.practice.distribution.mapper")
 @ComponentScan(basePackages = {"software.practice.distribution.controller","software.practice.distribution.service","software.practice.distribution.entity","software.practice.distribution.result","software.practice.distribution.mapper"})
 public class DistributionApplication {
 
+    @PostConstruct
+    void setDefaultTimezone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
         SpringApplication.run(DistributionApplication.class, args);
     }
-
 }
